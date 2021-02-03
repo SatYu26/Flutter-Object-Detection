bc8ba948734696823dbe421a45c0b3404c7a683a
SatYu26 <goyalsatyam8@gmail.com>
2021-02-04 00:26:12 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Object Detection Flutter APP
+
+
+## Machine Learning powered Android Application
+
+This application can detect objects in any of the three ways:
+  * Image choosen from Gallery
+  * From image taken within the App
+  * Real time in video stream
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:object_detection/realtime/live_camera.dart';
+import 'package:object_detection/static/static.dart';
+
+List<CameraDescription> cameras;
+
+Future<void> main() async {
+  // initialize the cameras when the app starts
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  // running the app
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Object Detector App"),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ButtonTheme(
+                minWidth: 170,
+                child: RaisedButton(
+                  child: Text("Detect in Image"),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => StaticImage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              ButtonTheme(
+                minWidth: 160,
+                child: RaisedButton(
+                  child: Text("Real Time Detection"),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LiveFeed(cameras),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
